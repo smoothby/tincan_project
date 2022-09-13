2674f84a51550355ff2ed0a8fe5bc0ac88d6e8bf
Matt Boothby <matt@boothby.me>
2022-09-13 15:22:10 -0400

    Initial Commit.

---
+++ index.php
@@ -0,0 +1,232 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>tincan</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+        integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700;900&family=Ubuntu:wght@400;700&display=swap"
+    rel="stylesheet">
+  <link rel="stylesheet" href="css/main.css">
+  <script src="https://kit.fontawesome.com/3005d6f471.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+<section id="title">
+  <div class="container-fluid">
+    <nav class="navbar navbar-expand-lg navbar-dark">
+      <a class="navbar-brand" href="">tincan</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
+              aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarToggler">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#footer">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#pricing">Pricing</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#cta">Download</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <div class="row">
+      <div class="col-lg-6">
+        <div>
+          <h1 class="title-heading1">Long shelf life, zero commitment.</h1>
+          <button type="button" class="btn btn-outline-light btn-lg btn-download"><i
+            class="fas fa-exclamation-circle"></i> Buy Now
+          </button>
+          <button type="button" class="btn btn-outline-light btn-lg btn-download"><i class="fas fa-clock"></i>
+            Buy Later
+          </button>
+        </div>
+      </div>
+      <div class="col-lg-6">
+        <div>
+          <img class="title-img" src="img/can-full.png" alt="the-can">
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="features">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="feature-box col-lg-4">
+        <i class="icon fas fa-list-ol fa-4x"></i>
+        <h3 class="features-heading3">Easy to use.</h3>
+        <p class="feature-text">All you need is an automatic or manual opener.</p>
+      </div>
+
+      <div class="feature-box col-lg-4">
+        <i class="icon far fa-gem fa-4x"></i>
+        <h3 class="features-heading3">Elite Clientele</h3>
+        <p class="feature-text">Even the best sometimes open a can or two, or have them opened for them.</p>
+      </div>
+
+      <div class="feature-box col-lg-4">
+        <i class="icon fas fa-check-circle fa-4x"></i>
+        <h3 class="features-heading3">Guaranteed to work.</h3>
+        <p class="feature-text">Or send you to the clinic for stitches if you mess it up.</p>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="testimonials">
+
+  <div id="testimonial-carousel" class="carousel slide" data-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active container-fluid">
+        <h2 class="testimonials-heading2">I thought all my food had spoiled but I was wrong! Thank you soooo much!</h2>
+        <img class="testimonial-img" src="img/woman-img.jpg" alt="woman-profile">
+        <p class="testimonial-name"><em>Tincanfan1964, Indiana</em></p>
+      </div>
+      <div class="carousel-item container-fluid">
+        <h2 class="testimonials-heading2">I never knew how much potted meat I could eat in one sitting, now I know
+          exactly how many cans!</h2>
+        <img class="testimonial-img" src="img/man-img.jpg" alt="man-profile">
+        <p class="testimonial-name"><em>SlamminSpammins, United Kingdom</em></p>
+      </div>
+    </div>
+    <a class="carousel-control-prev" href="#testimonial-carousel" role="button" data-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="sr-only">Previous</span>
+    </a>
+    <a class="carousel-control-next" href="#testimonial-carousel" role="button" data-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="sr-only">Next</span>
+    </a>
+  </div>
+
+</section>
+
+<section id="press">
+  <img class="press-logo" src="img/techcrunch.png" alt="tc-logo">
+  <img class="press-logo" src="img/tnw.png" alt="tnw-logo">
+  <img class="press-logo" src="img/bizinsider.png" alt="biz-insider-logo">
+  <img class="press-logo" src="img/mashable.png" alt="mashable-logo">
+</section>
+
+<section id="pricing">
+  <div class="container-fluid">
+    <h2 class="pricing-heading2">There's a Can for Every Food</h2>
+    <p>Quick, simple, affordable. Choose the can that's right for you.</p>
+
+    <div class="row">
+      <div class="pricing-column col-lg-4 col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="can-type">Small Can</h3>
+          </div>
+          <div class="card-body">
+            <h2 class="can-price">6 ounces</h2>
+            <p>170 g</p>
+            <p>5 x 2 tbsp. servings</p>
+            <p>Pet food / Pastes / Pâté</p>
+            <button type="button" class="btn btn-lg btn-block btn-light btn-pricing">Sign Up</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="pricing-column col-lg-4 col-md-6">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="can-type">Standard Can</h3>
+          </div>
+          <div class="card-body">
+            <h2 class="can-price">15 ounces</h2>
+            <p>425 g</p>
+            <p>3.5 x 1/2 cup servings</p>
+            <p>Beans / Sauces / Soups</p>
+            <button type="button" class="btn btn-lg btn-block btn-light btn-pricing">Sign Up</button>
+          </div>
+        </div>
+      </div>
+
+      <div class="pricing-column col-lg-4">
+        <div class="card">
+          <div class="card-header">
+            <h3 class="can-type">Large Can</h3>
+          </div>
+          <div class="card-body">
+            <h2 class="can-price">105 ounces</h2>
+            <p>3 kg</p>
+            <p>24 x 1/2 cup servings</p>
+            <p>Bulk items</p>
+            <button type="button" class="btn btn-lg btn-block btn-light btn-pricing">Sign Up</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section id="cta">
+  <div class="container-fluid">
+    <h3 class="cta-heading">Find the Right Can for Your Food Today.</h3>
+    <button type="button" class="btn btn-lg btn-dark cta-button"><i class="fas fa-exclamation-circle"></i> Buy Now
+    </button>
+    <button type="button" class="btn btn-lg btn-light cta-button"><i class="fas fa-clock"></i>
+      Buy Later
+    </button>
+  </div>
+</section>
+
+<footer id="footer">
+  <div class="container-fluid">
+    <a href="#"><i class="social-icon fab fa-facebook-square fa-2x"></i></a>
+    <a href="#"><i class="social-icon fab fa-twitter-square fa-2x"></i></a>
+    <a href="#"><i class="social-icon fab fa-instagram-square fa-2x"></i></a>
+    <a href="#"><i class="social-icon fas fa-envelope-square fa-2x"></i></a>
+
+    <p class="footer-text">&copy; 2022 <a href="https://www.boothby.me/" target="_blank">Matt Boothby</a>.
+    <p class="photo-credit"> Photo by <a
+      href="https://unsplash.com/@gabrielsilverio?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Gabriel
+      Silvério</a> on <a
+      href="/s/photos/gabriel-silv%C3%A9rio-fotografias?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
+      <br>
+      Photo by <a
+        href="https://unsplash.com/@dargonesti?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Ludovic
+        Migneault</a> on <a
+        href="/s/photos/portrait-man?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a>
+    </p>
+  </div>
+</footer>
+
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+        integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+        crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+        integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+        crossorigin="anonymous"></script>
+<script src="js/vendor/modernizr-3.11.2.min.js"></script>
+<script src="js/plugins.js"></script>
+<script src="js/main.js"></script>
+
+</body>
+
+</html>
